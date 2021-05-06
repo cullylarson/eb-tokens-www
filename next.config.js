48f60ddb28a004fb0bdbd71ad2fbbd53c9719d9a
Cully Larson <cully.larson@gmail.com>
2021-05-06 15:25:18 -0700

    Set lang attribute on html element.

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ module.exports = {
         webpack5: true,
     },
 
+	i18n: {
+		locales: ['en-US'],
+		defaultLocale: 'en-US',
+	},
+
     webpack(config, options) {
         // ENVIRONMENT VARIABLES
 
